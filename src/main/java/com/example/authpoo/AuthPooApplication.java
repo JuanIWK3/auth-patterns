cfebe75820644792bedb3aa5515a4c433d5ddf3b
juaniwk3 <juaniwk3@gmail.com>
2023-01-16 12:39:32 -0300

    new

---
+++ src/main/java/com/example/authpoo/AuthPooApplication.java
@@ -0,0 +1,13 @@
+package com.example.authpoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthPooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthPooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/authpoo/AuthPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authpoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthPooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

fe35f8e20afb4476ac2214b22b5dcafb7f4f5f31
juaniwk3 <juaniwk3@gmail.com>
2023-01-15 22:39:05 -0300

    with mongo

---
+++ src/main/java/com/example/auth/AuthPatternsApplication.java
@@ -0,0 +1,23 @@
+package com.example.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class AuthPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthPatternsApplication.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}

e486b3231a3ea5c48ca578dbf1491502a5db7c7f
juaniwk3 <juaniwk3@gmail.com>
2023-01-16 16:29:10 -0300

    auth route

--- src/main/java/com/example/authpoo/auth/AuthenticationService.java
+++ src/main/java/com/example/authpoo/auth/AuthenticationService.java
@@ -37,19 +37,19 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                request.getEmail(),
-                request.getEmail()
-        ));
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
 
-        var user = repository
-                .findByEmail(request.getEmail())
+        var user = repository.findByEmail(request.getEmail())
                 .orElseThrow();
 
-        var jwtToken = jwtService.generateToken(user);
 
-        return AuthenticationResponse
-                .builder()
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
     }
